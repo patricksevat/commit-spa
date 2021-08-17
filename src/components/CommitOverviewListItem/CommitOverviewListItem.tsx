d1d0d80439a8afb482b9c3fd8ada7b9f2dd174b9
patricksevat <patrick.sevat@rabobank.nl>
2021-08-17 19:04:21 +0200

    feat(): apply dirty hack to make table cell use ellipsis

--- src/components/CommitOverviewListItem/CommitOverviewListItem.tsx
+++ src/components/CommitOverviewListItem/CommitOverviewListItem.tsx
@@ -1,12 +1,15 @@
 import React, { FunctionComponent } from 'react';
 import { TableCell, TableRow } from '@material-ui/core';
+import './CommitOverviewListItem.scss'
 
 export const CommitOverviewListItem: FunctionComponent<ICommitOverviewListItemProps> = ({ message, date, author}) => {
   return (
     <TableRow>
-      <TableCell>{ message }</TableCell>
-      <TableCell>{ date || '' }</TableCell>
-      <TableCell>{ author }</TableCell>
+      <TableCell className={'commit-overview-list-item commit-overview-list-item__message'}>
+        <div className={'commit-overview-list-item__message--ellipsis'} title={message}>{ message }</div>
+      </TableCell>
+      <TableCell className={'commit-overview-list-item'}>{ date || '' }</TableCell>
+      <TableCell className={'commit-overview-list-item'}>{ author }</TableCell>
     </TableRow>
   )
 }
