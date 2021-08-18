01ddc0709a4c6183e991ac5f0785299bb058bb05
patricksevat <patrick.sevat@rabobank.nl>
2021-08-18 22:29:56 +0200

    feat(): layout for commit detail card

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -3,6 +3,9 @@ import { CommitContext } from '../../hooks/CommitContext';
 import { Error } from '../../components/Error/Error';
 import { TranslationContext } from '../../hooks/TranslationContext';
 import { useHistory } from 'react-router-dom';
+import { Avatar, Card, CardContent, CardHeader, Divider, IconButton, Typography } from '@material-ui/core';
+import { OpenInNew } from '@material-ui/icons';
+import './DetailPage.scss'
 
 export const DetailPage: FunctionComponent = () => {
   const history = useHistory();
@@ -19,9 +22,39 @@ export const DetailPage: FunctionComponent = () => {
     )
   }
 
+  // TODO move to own component
+  const message= selectedCommit.commit.message
+  const date= selectedCommit.commit.author.formattedDate
+  const author= selectedCommit.author.login
+  const avatar= selectedCommit.author.avatar_url
+
+  function handleIconButtonClick() {
+    window.open(selectedCommit?.html_url, '_blank')
+  }
+
   return (
-    <div>
-      detail page: {selectedCommit?.sha}
-    </div>
+    <Card className='detail-card'>
+      <CardHeader
+        title={author}
+        subheader={date}
+        avatar={
+          <Avatar alt={author} src={avatar}/>
+        }
+        action={
+          <IconButton aria-label="settings" onClick={handleIconButtonClick}>
+            <OpenInNew/>
+          </IconButton>
+        }
+      />
+      <CardContent>
+        <Typography variant='body2'>
+          { message }
+        </Typography>
+        <Divider className='detail-card__divider'/>
+        <Typography variant='caption'>
+          SHA: {selectedCommit.sha}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
